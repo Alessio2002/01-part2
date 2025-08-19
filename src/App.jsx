c55a6b9fafd690c869d778deb5be1e6252c28a82
Alessio2002 <alessio.zanasi02@gmail.com>
2025-08-19 10:18:54 +0300

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ const App = () => {
         <p>
           In vīcō nātālī meō quondam ambulāns, vaccam in forō venālem
           animadvertī. Cui pretium erat ducentōrum dēnāriōrum, quam tamen ēmī,
-          ut mihi domesticīsque māne cotīdiē lac suppeteret. Emendum mihi
-          videbātur, quod in oppidō meō vaccae omnīnō deerant. Sed, cum vīcīnus
-          quīdam ex proximō mihi vaccam suam obtulisset, ab emendā illā vaccā
-          desistendum putāvī; satis enim mihi id esse iūdicāvī. Carum quidem
-          erat bovem comparāre, sed necessārium, quoniam sine eā lac cotidie
-          habēre nōn poteram. Nunc vaccam possideō, quae mihi meīsque lac māne
-          cotidie praebeat; eam autem, quoniam mea facta est, summā cum
-          dīligentiā cūrāre dēbeō.
+          ut mihi domesticīsque māne cotīdiē lac suppeteret.
+        </p>
+        <p>
+          Emendum mihi videbātur, quod in oppidō meō vaccae omnīnō deerant. Sed,
+          cum vīcīnus quīdam ex proximō mihi vaccam suam obtulisset, ab emendā
+          illā vaccā desistendum putāvī; satis enim mihi id esse iūdicāvī. Carum
+          quidem erat bovem comparāre, sed necessārium, quoniam sine eā lac
+          cotidie habēre nōn poteram.
+        </p>{" "}
+        <p>
+          Nunc vaccam possideō, quae mihi meīsque lac māne cotidie praebeat; eam
+          autem, quoniam mea facta est, summā cum dīligentiā cūrāre dēbeō.
         </p>
       </h1>
     </div>
